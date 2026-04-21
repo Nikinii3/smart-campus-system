d0d3064ecf40f1d2c73cff4dd5f17f48c907e750
Nikinii3 <nikzylive@gmail.com>
2026-04-21 15:24:55 +0530

    feat: add Room POJO model with id, name, capacity and sensorIds fields

---
+++ src/main/java/org/smartcampus/model/Room.java
@@ -0,0 +1,64 @@
+package org.smartcampus.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Room {
+    private String id;  // The unique identifier
+    private String name; // Readable name
+    private int capacity;
+    private List<String> sensorIds = new ArrayList<>();
+
+    //Default constructor - jackson deserialization
+    public Room() {}
+
+    public Room(String id, String name, int capacity){
+        this.id = id;
+        this.name= name;
+        this.capacity= capacity;
+    }
+
+    //Getters and setters
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<String> getSensorIds() {
+        return sensorIds;
+    }
+
+    public void setSensorIds(List<String> sensorIds) {
+        this.sensorIds = sensorIds;
+    }
+
+    public void addSensorId(String sensorId) {
+        if(!this.sensorIds.contains(sensorId)) {
+            this.sensorIds.add(sensorId);
+        }
+    }
+
+    public void removeSensorId(String sensorId){
+        this.sensorIds.remove(sensorId);
+    }
+}
